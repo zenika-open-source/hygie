4b423ad0513eb66da858f90c147fc84b4834f051
TERRIER Bastien <bastien.terrier@gmail.com>
2019-02-15 10:57:34 +0100

    #21 - add .grenrc.js for layout

---
+++ .grenrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  template: {
+    commit: ({ message, url, author, name }) =>
+      `- [${message}](${url}) - ${author ? `@${author}` : name}`,
+    issue: "- {{labels}} {{name}} [{{text}}]({{url}})",
+    label: "[**{{label}}**]",
+    noLabel: "closed",
+    group: "\n#### {{heading}}\n",
+    changelogTitle: "# Changelog\n\n",
+    release: "## {{release}} ({{date}})\n{{body}}",
+    releaseSeparator: "\n---\n\n"
+  },
+  groupBy: {
+    "Enhancements:": ["enhancement", "internal"],
+    "Bug Fixes:": ["bug"]
+  }
+};
