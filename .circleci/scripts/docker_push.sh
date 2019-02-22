c7065fc14a94a9c0d8f55369d3016fd746973438
TERRIER Bastien <bastien.terrier@gmail.com>
2019-02-22 14:02:09 +0100

    fix: docker build in circleci

--- .circleci/scripts/docker_push.sh
+++ .circleci/scripts/docker_push.sh
@@ -1,11 +1,13 @@
 VERSION=$1
 VERSION="${VERSION:1}"
 
+cd ../..
 echo "> build"
-docker build -t my-webhook:$VERSION ../../.
+docker build -t my-webhook:$VERSION .
 echo "> login"
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 echo "> tag"
 docker tag my-webhook:$VERSION dxdeveloperexperience/git-webhooks:$VERSION
 echo "> push"
 docker push dxdeveloperexperience/git-webhooks:$VERSION
+cd .circleci/scripts
