2527672fa339a87bb6550b08fe8e205b4ff83fbd
TERRIER Bastien <bastien.terrier@gmail.com>
2019-03-01 13:17:28 +0100

    feat: add labeled groups for release changelog (#61)

--- .grenrc.js
+++ .grenrc.js
@@ -2,16 +2,19 @@ module.exports = {
   template: {
     commit: ({ message, url, author, name }) =>
       `- [${message}](${url}) - ${author ? `@${author}` : name}`,
-    issue: "- {{labels}} {{name}} [{{text}}]({{url}})",
-    label: "[**{{label}}**]",
-    noLabel: "closed",
-    group: "\n#### {{heading}}\n",
-    changelogTitle: "# Changelog\n\n",
-    release: "## {{release}} ({{date}})\n{{body}}",
-    releaseSeparator: "\n---\n\n"
+    issue: '- {{labels}} {{name}} [{{text}}]({{url}})',
+    label: '[**{{label}}**]',
+    noLabel: 'closed',
+    group: '\n#### {{heading}}\n',
+    changelogTitle: '# Changelog\n\n',
+    release: '## {{release}} ({{date}})\n{{body}}',
+    releaseSeparator: '\n---\n\n',
   },
   groupBy: {
-    "Enhancements:": ["enhancement", "internal"],
-    "Bug Fixes:": ["bug"]
-  }
+    'Enhancements:': ['enhancement', 'internal'],
+    'Bug Fixes:': ['bug'],
+    'Rules:': ['rules'],
+    'Post-Actions:': ['post actions'],
+    'Others:': ['question', 'build'],
+  },
 };
