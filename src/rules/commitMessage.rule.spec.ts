91bd64370567b41ac30cca709275bdc15fa73f44
TERRIER Bastien <bastien.terrier@gmail.com>
2019-03-08 08:59:47 +0100

    fix(tdd): commitMessageRule spec

--- src/rules/commitMessage.rule.spec.ts
+++ src/rules/commitMessage.rule.spec.ts
@@ -75,19 +75,25 @@ describe('RulesService', () => {
         branch: 'test_webhook',
         commits: [
           {
+            status: 'Success',
+            success: true,
             sha: '1',
             message: 'fix: readme (#12)',
-            matches: ['fix: readme (#12)', 'fix', undefined, '(#12)'],
+            matches: ['fix: readme (#12)', 'fix', null, '(#12)'],
           },
           {
+            status: 'Success',
+            success: true,
             sha: '2',
             message: 'feat(test): tdd (#34)',
             matches: ['feat(test): tdd (#34)', 'feat', '(test)', '(#34)'],
           },
           {
+            status: 'Success',
+            success: true,
             sha: '3',
             message: 'docs: gh-pages',
-            matches: ['docs: gh-pages', 'docs', undefined, undefined],
+            matches: ['docs: gh-pages', 'docs', null, null],
           },
         ],
       };
