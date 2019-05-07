d364f79ba684757f694a4f16f245e8841005242e
TERRIER Bastien <bastien.terrier@gmail.com>
2019-05-07 09:46:52 +0200

    v0.0.4

--- src/generator/generateYAMLSchema.ts
+++ src/generator/generateYAMLSchema.ts
@@ -1,5 +1,6 @@
 import { getYAMLSchema } from './getYAMLSchema';
 
 const fs = require('fs-extra');
+const path = require('path');
 
-fs.writeJsonSync('./rules-schema.json', getYAMLSchema());
+fs.writeJsonSync(path.resolve(__dirname, 'rules-schema.json'), getYAMLSchema());
