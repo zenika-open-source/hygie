f0b63f0d44e2465c8856948ad5c6da693dfddab0
TERRIER Bastien <bastien.terrier@gmail.com>
2019-05-27 17:28:24 +0200

    test(check): add Unit Test for Check methods

--- src/check/utils.spec.ts
+++ src/check/utils.spec.ts
@@ -1,8 +1,9 @@
-import { checkInternet } from './utils';
 import { Test, TestingModule } from '@nestjs/testing';
 import { HttpService } from '@nestjs/common';
 import { MockHttpService } from '../__mocks__/mocks';
 import { throwError } from 'rxjs';
+import { logger } from '../logger/logger.service';
+import { Check } from './utils';
 
 describe('Check Utils', () => {
   let app: TestingModule;
@@ -15,15 +16,56 @@ describe('Check Utils', () => {
 
     httpService = app.get(HttpService);
   });
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   describe('CheckInternet', () => {
     it('Should return true', async () => {
-      expect(await checkInternet(httpService)).toBe(true);
+      expect(await Check.checkInternet(httpService)).toBe(true);
     });
     it('Should return false', async () => {
       httpService.get = jest
         .fn()
         .mockImplementationOnce(() => throwError('Error 404'));
-      expect(await checkInternet(httpService)).toBe(false);
+      expect(await Check.checkInternet(httpService)).toBe(false);
+    });
+  });
+
+  describe('checkIfFileExist', () => {
+    it('Should return true', async () => {
+      const fs = require('fs-extra');
+      fs.existsSync = jest.fn().mockReturnValue(true);
+
+      logger.error = jest.fn().mockName('logger.error');
+
+      expect(Check.checkIfFileExist('some/file.ext')).toBe(true);
+      expect(logger.error).not.toBeCalled();
+    });
+    it('Should return false', async () => {
+      const fs = require('fs-extra');
+      fs.existsSync = jest.fn().mockReturnValue(false);
+
+      logger.error = jest.fn().mockName('logger.error');
+
+      expect(Check.checkIfFileExist('some/file.ext')).toBe(false);
+      expect(logger.error).toBeCalled();
+    });
+  });
+
+  describe('checkNeededFiles', () => {
+    it('Should return true', async () => {
+      Check.checkIfFileExist = jest.fn().mockReturnValue(true);
+      expect(Check.checkNeededFiles(['some/file.ext', 'other/file.ext'])).toBe(
+        true,
+      );
+    });
+    it('Should return false', async () => {
+      Check.checkIfFileExist = jest.fn().mockReturnValue(false);
+      expect(Check.checkNeededFiles(['some/file.ext', 'other/file.ext'])).toBe(
+        false,
+      );
     });
   });
 });
