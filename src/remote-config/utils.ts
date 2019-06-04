bfc44205611f78ebe967f086727daed04efb3108
TERRIER Bastien <bastien.terrier@gmail.com>
2019-06-04 10:20:31 +0200

    feat(): temporary solution to use the .rulesrc file of the default branch (#203)

--- src/remote-config/utils.ts
+++ src/remote-config/utils.ts
@@ -82,7 +82,10 @@ export class RemoteConfigUtils {
     projectURL: string,
     filename: string,
     branch: string = 'master',
+    defaultBranch?: string,
   ): Promise<string> {
+    logger.info(branch + ' - ' + defaultBranch);
+
     return new Promise(async (resolve, reject) => {
       const whichGit: GitTypeEnum = this.getGitType(projectURL);
 
@@ -118,23 +121,46 @@ export class RemoteConfigUtils {
         return;
       }
 
+      // TEMPORARY
+      let data: string = '';
+      try {
+        const defaultFilePath: string = this.getGitRawPath(
+          whichGit,
+          projectURL,
+          `.git-webhooks/${filename}`,
+          defaultBranch,
+        );
+        data = await httpService
+          .get(defaultFilePath)
+          .toPromise()
+          .then(response => response.data)
+          .catch(error => {
+            throw new Error(error);
+          });
+      } catch (e) {
+        data = '';
+      }
+
       // Download file
       await httpService
         .get(rulesFilePath)
         .pipe(
           catchError(err => {
             if (filename === Constants.rulesExtension) {
+              if (defaultBranch !== branch && data !== '') {
+                return of({ data });
+              }
+
               logger.warn(
                 `No ${
                   Constants.rulesExtension
                 } file founded. Using the default one.`,
                 { project: projectURL, location: 'downloadRulesFile' },
               );
-              return of({
-                data: fs.readFileSync(
-                  path.join(__dirname, `../rules/${Constants.rulesExtension}`),
-                ),
-              });
+              data = fs.readFileSync(
+                path.join(__dirname, `../rules/${Constants.rulesExtension}`),
+              );
+              return of({ data });
             } else {
               return throwError(`${rulesFilePath} do not exist!`);
             }
