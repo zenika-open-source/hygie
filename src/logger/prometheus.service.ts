f55627cdc55b1ba1d2fdd3089a185a511b678ebd
TERRIER Bastien <bastien.terrier@gmail.com>
2019-07-02 14:33:16 +0200

    test

--- src/logger/prometheus.service.ts
+++ src/logger/prometheus.service.ts
@@ -16,11 +16,15 @@ export class PrometheusService {
     }
     logger.info('Apres', { location: 'Prometheus' });
 
-    this.httpRequestDurationMicroseconds = new this.Prometheus.Histogram({
-      name: 'http_request_duration_ms',
-      help: 'Duration of HTTP requests in ms',
-      labelNames: ['method', 'route', 'code'],
-      buckets: [50, 100, 200, 300, 400, 500, 750, 1000], // buckets for response time from 50ms to 1000ms
-    });
+    try {
+      this.httpRequestDurationMicroseconds = new this.Prometheus.Histogram({
+        name: 'http_request_duration_ms',
+        help: 'Duration of HTTP requests in ms',
+        labelNames: ['method', 'route', 'code'],
+        buckets: [50, 100, 200, 300, 400, 500, 750, 1000], // buckets for response time from 50ms to 1000ms
+      });
+    } catch (e) {
+      logger.error(e, { location: 'Prometheus' });
+    }
   }
 }
