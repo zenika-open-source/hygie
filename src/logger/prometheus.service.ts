ecdc9aac07abefd1f91df95ab5cc72306617313b
Bastien TERRIER <bastien.terrier@gmail.com>
2019-07-02 16:42:45 +0200

    fix: Prometheus CPU infos
    Use our own version of prom-client hosted at @dxdeveloperexperience/prom-client that do not contain CPU informations

--- src/logger/prometheus.service.ts
+++ src/logger/prometheus.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { logger } from './logger.service';
 
 @Injectable()
 export class PrometheusService {
@@ -6,13 +7,22 @@ export class PrometheusService {
   httpRequestDurationMicroseconds;
   Prometheus;
   constructor() {
-    this.Prometheus = require('prom-client');
-    this.Prometheus.collectDefaultMetrics();
-    this.httpRequestDurationMicroseconds = new this.Prometheus.Histogram({
-      name: 'http_request_duration_ms',
-      help: 'Duration of HTTP requests in ms',
-      labelNames: ['method', 'route', 'code'],
-      buckets: [50, 100, 200, 300, 400, 500, 750, 1000], // buckets for response time from 50ms to 1000ms
-    });
+    this.Prometheus = require('@dxdeveloperexperience/prom-client');
+    try {
+      this.Prometheus.collectDefaultMetrics();
+    } catch (err) {
+      logger.error(err, { location: 'Prometheus 1' });
+    }
+
+    try {
+      this.httpRequestDurationMicroseconds = new this.Prometheus.Histogram({
+        name: 'http_request_duration_ms',
+        help: 'Duration of HTTP requests in ms',
+        labelNames: ['method', 'route', 'code'],
+        buckets: [50, 100, 200, 300, 400, 500, 750, 1000], // buckets for response time from 50ms to 1000ms
+      });
+    } catch (e) {
+      logger.error(e, { location: 'Prometheus 2' });
+    }
   }
 }
