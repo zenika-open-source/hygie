2a837437470b63814c71e5c7f8412a8ff746fbfd
TERRIER Bastien <bastien.terrier@gmail.com>
2019-07-11 17:57:19 +0200

    fix: build

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,6 @@ import { DataAccessService } from './data_access/dataAccess.service';
 import { Check } from './check/utils';
 import { AllExceptionsFilter } from './exceptions/allExceptionFilter';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
