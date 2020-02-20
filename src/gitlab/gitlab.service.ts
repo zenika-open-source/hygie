846af19ab9db97371faf13693e2529a529e34261
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2020-02-20 14:54:35 +0100

    Add Pending status (#332)
    * feat: use alias for modules
    
    * feat: manage WIP
    
    * fix: solve unit test
    
    Co-authored-by: Bastien TERRIER <bastien.terrier@gmail.com>

--- src/gitlab/gitlab.service.ts
+++ src/gitlab/gitlab.service.ts
@@ -87,7 +87,7 @@ export class GitlabService implements GitServiceInterface {
         ),
         target_url: gitCommitStatusInfos.targetUrl,
         description: gitCommitStatusInfos.descriptionMessage,
-        context: process.env.APPLICATION_NAME + '/commit',
+        context: process.env.APPLICATION_NAME + gitCommitStatusInfos.context,
       },
     };
 
