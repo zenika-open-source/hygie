e8f1f306b65fe7735ae263accfe11a60a354ec6e
Bastien TERRIER <bastien.terrier@gmail.com>
2020-04-30 17:40:17 +0200

    feat: add dotenv config

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,8 @@ import { AllExceptionsFilter } from './exceptions/allExceptionFilter';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 
+require('dotenv').config();
+
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(join(__dirname, '..', 'public'));
